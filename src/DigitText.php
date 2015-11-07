551f133b4b3e78d9c4d7132e3897a11b3be9a2ae
Helldar <helldar@ai-rus.com>
2015-11-08 02:57:07 +0800

    Change class

--- src/DigitText.php
+++ src/DigitText.php
@@ -4,25 +4,26 @@ namespace Helldar\DigitText;
 
 class DigitText
 {
+    private $texts = [
+        '0' => 'ноль',
+        '1' => 'один',
+        '2' => 'два',
+        '3' => 'три',
+    ];
+
+    /**
+     *
+     * @param type $digit
+     * @return type
+     */
     public function getText($digit = null)
     {
         if (is_null($digit)) {
-            return $this->getArray(0);
+            return $this->texts[0];
         }
 
         if ($digit > 0 && $digit < 10) {
-            return $this->getArray($digit);
+            return $this->texts[$digit];
         }
     }
-
-    private function getArray($digit = 0)
-    {
-        return [
-            '0' => 'ноль',
-            '1' => 'один',
-            '2' => 'два',
-            '3' => 'три',
-        ];
-    }
-
 }
