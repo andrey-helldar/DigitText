baf9ff529f3ffb9d8c6756e8b0c0467373ffd0ff
Helldar <helldar@ai-rus.com>
2015-11-08 02:21:11 +0800

    Adding Class

---
+++ src/DigitText.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Helldar;
+
+class DigitText
+{
+    public function getText($digit = null)
+    {
+        if ( is_null($digit) ) {
+            return $this->getArray(0);
+        }
+
+        if ( $digit > 0 && $digit < 10 ) {
+            return $this->getArray($digit);
+        }
+    }
+
+    private function getArray($digit = 0)
+    {
+        return [
+            '0' => 'ноль',
+            '1' => 'один',
+            '2' => 'два',
+            '3' => 'три',
+        ];
+    }
+
+}
