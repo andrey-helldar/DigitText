39ef76e4278f34cc5a307b687dd4dcd51d17a587
Andrey Helldar <helldar@ai-rus.com>
2015-11-09 20:25:09 +0800

    Adding the locales.

---
+++ lang/ru/digittext.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Andrey Helldar <helldar@ai-rus.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+return [
+    0 => [
+        0  => '',
+        1  => 'один',
+        2  => 'два',
+        3  => 'три',
+        4  => 'четыре',
+        5  => 'пять',
+        6  => 'шесть',
+        7  => 'семь',
+        8  => 'восемь',
+        9  => 'девять',
+        10 => 'десять',
+        11 => 'одиннадцать',
+        12 => 'двенадцать',
+        13 => 'тринадцать',
+        14 => 'четырнадцать',
+        15 => 'пятнадцать',
+        16 => 'шестнадцать',
+        17 => 'семнадцать',
+        18 => 'восемнадцать',
+        19 => 'девятнадцать',
+    ],
+    1 => [
+        2 => 'двадцать',
+        3 => 'тридцать',
+        4 => 'сорок',
+        5 => 'пятьдесят',
+        6 => 'шестьдесят',
+        7 => 'семьдесят',
+        8 => 'восемьдесят',
+        9 => 'девяносто',
+    ],
+    2 => [
+        0 => '',
+        1 => 'сто',
+        2 => 'двести',
+        3 => 'триста',
+        4 => 'четыреста',
+        5 => 'пятьсот',
+        6 => 'шестьсот',
+        7 => 'семьсот',
+        8 => 'восемьсот',
+        9 => 'девятьсот',
+    ],
+    3 => [
+        0  => '',
+        1  => 'одна',
+        2  => 'две',
+        3  => 'три',
+        4  => 'четыре',
+        5  => 'пять',
+        6  => 'шесть',
+        7  => 'семь',
+        8  => 'восемь',
+        9  => 'девять',
+        10 => 'десять',
+        11 => 'одиннадцать',
+        12 => 'двенадцать',
+        13 => 'тринадцать',
+        14 => 'четырнадцать',
+        15 => 'пятнадцать',
+        16 => 'шестнадцать',
+        17 => 'семнадцать',
+        18 => 'восемнадцать',
+        19 => 'девятнадцать',
+    ],
+    4 => [
+        0 => '',
+        2 => 'двадцать',
+        3 => 'тридцать',
+        4 => 'сорок',
+        5 => 'пятьдесят',
+        6 => 'шестьдесят',
+        7 => 'семьдесят',
+        8 => 'восемьдесят',
+        9 => 'девяносто',
+    ],
+    5 => [
+        0 => '',
+        1 => 'сто',
+        2 => 'двести',
+        3 => 'триста',
+        4 => 'четыреста',
+        5 => 'пятьсот',
+        6 => 'шестьсот',
+        7 => 'семьсот',
+        8 => 'восемьсот',
+        9 => 'девятьсот',
+    ]
+];
