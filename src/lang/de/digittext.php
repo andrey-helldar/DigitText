46780841f012fea99221fefa27f1581d0c69a5d2
Andrey Helldar <helldar@ai-rus.com>
2015-11-10 14:50:50 +0800

    Added German localization

---
+++ src/lang/de/digittext.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Andrey Helldar <helldar@ai-rus.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+return [
+    0           => [
+        0  => '',
+        1  => 'one',
+        2  => 'two',
+        3  => 'three',
+        4  => 'four',
+        5  => 'five',
+        6  => 'six',
+        7  => 'seven',
+        8  => 'eight',
+        9  => 'nine',
+        10 => 'ten',
+        11 => 'eleven',
+        12 => 'twelve',
+        13 => 'thirteen',
+        14 => 'fourteen',
+        15 => 'fifteen',
+        16 => 'sixteen',
+        17 => 'seventeen',
+        18 => 'eighteen',
+        19 => 'nineteen',
+    ],
+    1           => [
+        2 => 'twenty',
+        3 => 'thirty',
+        4 => 'forty',
+        5 => 'fifty',
+        6 => 'sixty',
+        7 => 'seventy',
+        8 => 'eighty',
+        9 => 'ninety',
+    ],
+    2           => [
+        0 => '',
+        1 => 'one hundred',
+        2 => 'two hundred',
+        3 => 'three hundred',
+        4 => 'four hundred',
+        5 => 'five hundred',
+        6 => 'six hundred',
+        7 => 'seven hundred',
+        8 => 'eight hundred',
+        9 => 'nine hundred',
+    ],
+    3           => [
+        0  => '',
+        1  => 'one',
+        2  => 'two',
+        3  => 'three',
+        4  => 'four',
+        5  => 'five',
+        6  => 'six',
+        7  => 'seven',
+        8  => 'eight',
+        9  => 'nine',
+        10 => 'ten',
+        11 => 'eleven',
+        12 => 'twelve',
+        13 => 'thirteen',
+        14 => 'fourteen',
+        15 => 'fifteen',
+        16 => 'sixteen',
+        17 => 'seventeen',
+        18 => 'eighteen',
+        19 => 'nineteen',
+    ],
+    4           => [
+        0 => '',
+        2 => 'twenty',
+        3 => 'thirty',
+        4 => 'forty',
+        5 => 'fifty',
+        6 => 'sixty',
+        7 => 'seventy',
+        8 => 'eighty',
+        9 => 'ninety',
+    ],
+    5           => [
+        0 => '',
+        1 => 'one hundred',
+        2 => 'two hundred',
+        3 => 'three hundred',
+        4 => 'four hundred',
+        5 => 'five hundred',
+        6 => 'six hundred',
+        7 => 'seven hundred',
+        8 => 'eight hundred',
+        9 => 'nine hundred',
+    ],
+    'currency'  => [
+        'int'      => 'dollars',
+        'fractal'  => 'cents',
+        'position' => 'after',
+    ],
+    'zero'      => 'zero',
+    'thousands' => [
+        0 => 'thousands',
+        1 => 'thousand',
+        2 => 'thousands',
+    ],
+    'millions'  => [
+        0 => 'million',
+        1 => 'million',
+        2 => 'million',
+    ],
+];
